4b5817defbfa751a2ea81b51f1c703454999af64
Olivia Ih <ih.olivia.e@gmail.com>
2024-05-15 20:47:57 -0700

    implement partial indexes with merging, search engine with text interface

---
+++ indexer.py
@@ -0,0 +1,152 @@
+import os
+from bs4 import BeautifulSoup
+import json
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from collections import defaultdict
+from heapq import heappop, heappush
+
+BATCH_SIZE = 2000     # number of documents per partial inverted index
+DATA_DIR = "DEV"
+INDEXES_DIR = "indexes"
+
+
+# INVERTED INDEX STRUCTURE
+# {string: [postings]}
+#   posting tuple: (doc_id, tf_idf)
+inverted_index = defaultdict(list)  # DEPRECATED; FOR MILESTONE 1 
+doc_id_map = {} # key = integer id, value  = file path
+
+class Posting:
+    def __init__(self, docid, tfidf):
+        self.docid = docid
+        self.tfidf = tfidf  # use freq counts for now
+
+    def __str__(self):
+        # return f'({self.docid}, {self.tfidf})'
+        return f'({self.docid}, {self.tfidf})'
+
+# returns dictionary {token: frequency}
+def computeWordFrequencies(tokens: list):
+    freqs = {}
+
+    for token in tokens:
+        if token in freqs:
+            freqs[token] += 1   # increment count if token already seen
+        else:
+            freqs[token] = 1    # initialize count of new token to 1
+
+    return freqs
+
+# returns the url and tokens 
+def _tokenize_file(file_path):
+    tokens = None
+
+    with open(file_path) as f:
+        for line in f:
+            # extract json
+            line_contents = json.loads(line)
+
+            url = line_contents['url']
+            content = line_contents['content']
+
+            # beautiful soup the content
+            soup = BeautifulSoup(content, features='xml')
+
+            # tokenize the page content from soup
+            tokens = word_tokenize(soup.get_text())
+        
+    return url, tokens
+
+
+def create_inverted_index():
+
+    # TO DO:
+    # - COSINE SIMILARITY METHOD FOR TF-IDF
+
+    doc_id_counter = 0
+    
+    # create partial index and doc counter to create index of size [BATCH_SIZE]
+    partial_inv_index = defaultdict(list)
+    doc_batch_counter = 0
+    num_batches = 0     
+    
+    ps = PorterStemmer()
+    # Iterate over subfolders in directory
+    for path, folders, _ in os.walk(DATA_DIR):
+        for folder_name in folders:
+            for file in os.listdir(f"{path}/{folder_name}"):
+            
+                file_path = f"{path}/{folder_name}/{file}"
+                print(file_path)   
+                             
+                # tokenize file's HTML content
+                url, tokens = _tokenize_file(file_path)
+                tokens = [ps.stem(token) for token in tokens]
+                # print(tokens)
+
+                # create doc id for document
+                doc_id_map[doc_id_counter] = url
+                doc_id_counter += 1
+                
+                # compute word frequencies of tokens
+                freqs = computeWordFrequencies(tokens)
+
+                # add to partial inverted index
+                for token, freq in freqs.items():
+                    partial_inv_index[token].append(Posting(doc_id_counter, freq))
+                
+                # add to main inverted index for testing purposes - REMOVE BEFORE SUBMITTING
+                for token, freq in freqs.items():
+                    inverted_index[token].append(Posting(doc_id_counter, freq))
+
+                doc_batch_counter += 1
+
+                if doc_batch_counter == BATCH_SIZE:
+                    num_batches += 1
+
+                    # write the partial index to disk in alphabetical order of tokens
+                    # reset doc counter and continue going through files
+                    
+                    # write partial_inverted_index to a file
+                    os.makedirs(INDEXES_DIR, exist_ok=True)   # create indexes folder if it doesn't exist
+
+                    with open(f'{INDEXES_DIR}/inverted_index{num_batches}.txt', 'w') as f:
+                        for token, postings in sorted(partial_inv_index.items(), key=lambda x: x[0]):
+                            f.write(f'{token}: {[eval(str(posting)) for posting in postings]}\n')
+                    
+                    # reset counter and index
+                    partial_inv_index = defaultdict(list)
+                    doc_batch_counter = 0
+                           
+           # break   # REMOVE - FOR TESTING
+           
+    # write main inverted index to disk - REMOVE LATER
+    with open(f'inverted_index.txt', 'w') as f:
+        for token, postings in sorted(inverted_index.items(), key=lambda x: x[0]):
+            f.write(f'{token}: {[eval(str(posting)) for posting in postings]}\n')
+
+    # write results to file - REMOVE BEFORE SUBMITTING
+    with open('milestone1_report2.txt', 'w') as f:
+        # get number of documents from doc_id_counter
+        f.write(f"Number of documents: {doc_id_counter}\n")
+        
+        # get number of unique tokens from len(inverted_index.keys())
+        f.write(f"Number of unique tokens: {len(inverted_index.keys())}\n")
+
+        # get total size (in KB) of file using os.path.getsize()
+        size = os.path.getsize('inverted_index.txt') / 1000    
+        f.write(f"Size of the inverted index (in KB): {size}")
+    
+    
+def write_doc_id_map_to_disk():
+    with open(f'document_id_map.txt', 'w') as f:
+        for doc_id, path in doc_id_map.items():
+            f.write(f'{doc_id}: {path}\n')
+            
+
+if __name__ == '__main__':
+    create_inverted_index()
+    # print(inverted_index)
+    write_doc_id_map_to_disk()
+
---
+++ searcher.py
@@ -0,0 +1,106 @@
+from indexer import *
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+
+DOC_ID_FILE = 'document_id_map.txt'
+
+doc_id_map = {}
+
+def _get_posting_list_intersection(token1_postings, token2_postings):
+    t1_index = 0
+    t2_index = 0
+    intersection = []
+
+    num_t1_postings = len(token1_postings)
+    num_t2_postings = len(token2_postings)
+
+    while t1_index < num_t1_postings and t2_index < num_t2_postings:
+        if token1_postings[t1_index].docid < token2_postings[t2_index].docid:
+            t1_index += 1
+        elif token2_postings[t2_index].docid < token1_postings[t1_index].docid:
+            t2_index += 1
+        else:
+            intersection.append(token2_postings[t2_index])
+            t1_index += 1
+            t2_index += 1
+    
+    return intersection
+
+# loads document id map from disk into global instance
+def _get_doc_id_map_from_disk():
+    with open(DOC_ID_FILE, 'r') as f:
+        for line in f:
+            id, url = line.split(': ')
+            doc_id_map[int(id)] = url.rstrip()
+
+
+# Do any preprocessing to the query text (tokenizing, capitalization, stemming, etc)
+def preprocess_query(query):
+    query_tokens = word_tokenize(query)
+    # use porter stemming on tokens
+    ps = PorterStemmer()
+    query_tokens = [ps.stem(token) for token in query_tokens]
+
+    return query_tokens
+
+# Get and return the merged postings list for each of the query tokens
+def merge_indexes(query_tokens):
+    query_token_postings = defaultdict(list)
+
+    # get postings lists for each token ()
+    #   merge postings lists from the partial indexes into one large inverted index for token t
+    for file in os.listdir(INDEXES_DIR):
+        with open(f'{INDEXES_DIR}/{file}', 'r') as f:
+            for line in f:
+                token, postings = line.split(': ')
+
+                if token in query_tokens:
+                    postings_list_as_tuples = eval(postings)
+                    postings_list_as_posting = [Posting(x[0], x[1]) for x in postings_list_as_tuples]
+                    query_token_postings[token].extend(postings_list_as_posting)
+
+    return query_token_postings
+
+
+# Perform a boolean AND search on the merged token lists (i.e. find intersection of doc IDs)
+def boolean_and_search(query_token_postings):
+    query_token_postings = list(query_token_postings.values())
+    running_intersection: list[Posting] = query_token_postings[0]
+
+    for posting in query_token_postings[1:]:
+        running_intersection = _get_posting_list_intersection(running_intersection, posting)
+
+    print([posting.docid for posting in running_intersection])
+    print(list(doc_id_map.items())[:10])
+    
+    return [posting.docid for posting in running_intersection]  
+
+def _print_results(urls):
+    for i, url in enumerate(urls[:5]):
+        print(f"({i+1}): {url}\n")
+
+def handle_query(query):
+    query_tokens = preprocess_query(query)
+    merged_posting_lists = merge_indexes(query_tokens)
+    bool_and_search_results = boolean_and_search(merged_posting_lists)
+    
+    # look up urls from doc id file
+    urls = []
+    for doc_id in bool_and_search_results:
+        urls.append(doc_id_map[doc_id])
+
+    _print_results(urls)
+
+def run_search_engine():
+    _get_doc_id_map_from_disk()
+
+    while True:
+        query = input("Enter query (q: quit): ")
+        if query == "q":
+            return
+        else:
+            handle_query(query)
+
+
+if __name__ == "__main__":
+    run_search_engine()
